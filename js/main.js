11ea4dfccd42cbd721eda069bbef48cdc074754d
Mhamudulislam <141499999+Mhamudulislam@users.noreply.github.com>
2023-11-23 20:45:27 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1048 @@
+// for_textChange
+function forOnclick()
+{document.getElementById('changg').innerHTML="hello World";}
+
+
+// forDate
+function chdate()
+{document.getElementById("forDate").innerHTML=Date();}
+
+
+
+// for_colorChange
+function colorCn()
+{document.getElementById('colcoler').style.color="blue";}
+
+
+// for_colorChange
+function colorCy()
+{document.getElementById('colcoler').style.color="yellow";}
+
+
+
+
+
+// imageChange
+function imgchang1()
+{document.getElementById('imgChange').src="for_anime/bird1.png";}
+function imgchang2()
+{document.getElementById('imgChange').src="for_anime/bird2.png";}
+
+
+
+// header_Line_Changer
+function changHed()
+{document.getElementsByClassName('header1')[3].innerHTML="hello world";}
+
+
+
+// header_Line_Changer
+function chaneHd()
+{document.getElementsByClassName('hed1')[0].innerHTML="hello world";}
+
+
+
+// header_Line_Changer
+function chaneH()
+{document.getElementsByClassName('hed3')[1].innerHTML="hello world";}
+
+
+// pluse
+var num1=50;
+var num2=50;
+var result=  num1 + num2;
+function valuShow()
+{document.getElementById('for_Valu').innerHTML=result;}
+
+
+// same
+let numben1, number2;
+numben1=10;
+number2=20;
+numben1 +=number2
+function forbt()
+{document.getElementById('for_b').innerHTML=numben1;}
+
+
+
+// alart_messese
+
+function foralar() 
+{alert('Hi there!');}
+
+
+
+// hovereffect
+function hoverCall()
+{document.getElementById('box1').style.display="none";}
+
+
+
+
+// hverOut
+function hoverMo()
+{document.getElementById('box2').style.display="none";}
+
+
+
+
+
+// hverout_click come
+function comeBox2()
+{document.getElementById('box3').style.display="none";}
+function box2Com()
+{document.getElementById('box3').style.display="block";}
+
+
+
+
+// hverin_click come
+function comeBox4()
+{document.getElementById('box4').style.display="none";}
+function box4Com()
+{document.getElementById('box4').style.display="block";}
+
+
+
+
+// hverin_click come
+function textF()
+{document.getElementById('textOne').innerHTML="imIn hello world";}
+function textS()
+{document.getElementById('textOne').innerHTML="imOut hello world";}
+
+
+
+
+// numbergroup
+function forMove()
+{document.getElementById('chanV').innerHTML++}
+
+
+// box colorchange
+function cllickBC()
+{document.getElementById('box5').style.background="red";}
+
+
+
+// box colorchange 2 button
+function cllickBo()
+{document.getElementById('box6').style.background="red";}
+function cllickBt()
+{document.getElementById('box6').style.background="black";}
+
+
+
+// font_change
+function fontCh()
+{document.getElementById('forFont').style.fontSize="100px";}
+
+
+
+
+// width_change
+function widtCh()
+{document.getElementById('box7').style.width="100px";}
+function widtChh()
+{document.getElementById('box7').style.width="700px";}
+
+
+
+
+
+// height_change
+function heigCh()
+{document.getElementById('box8').style.height="100px";}
+function heighChh()
+{document.getElementById('box8').style.height="500px";}
+
+
+
+
+
+
+// display_none
+function desplN()
+{document.getElementById('box9').style.display="none";}
+function desplB()
+{document.getElementById('box9').style.display="block";}
+
+
+// consol test
+
+function consolFor()
+{console.log("imError");}
+
+
+
+
+// alart test
+function alarFor()
+{alert(50 + 50);}
+
+
+
+
+
+// consol calculetion
+function coscFor()
+{console.log(200 + 200);}
+
+
+
+
+
+// practic1
+let x,y,z;
+x=50;
+y=50;
+z = x + y;
+function xyz()
+{document.getElementById('forPractis').innerHTML=z;}
+
+
+
+// paractic2
+let a,b,c;
+a = 10; b = 10; c = a + b;
+function abc()
+{document.getElementById('forPractis2').innerHTML=c;}
+
+
+
+
+
+// paractic3
+function xyz2()
+{document.getElementById('forPractis3').innerHTML=(5 + 5) * 10;}
+
+
+
+
+// paractic3
+function calkuleTion()
+{document.getElementById('forPractis4').innerHTML=20.70;}
+
+
+
+
+// paractic4
+let e, f;
+e = 5;
+f = 6;
+function easyWay()
+{document.getElementById('easyCal').innerHTML=e+f;}
+
+
+
+
+// example
+let p;
+p=5;
+function easyWay2()
+{document.getElementById('easyCal2').innerHTML=p;}
+
+
+
+
+
+// example
+function easyWay3()
+{document.getElementById('easyCal3').innerHTML="any1"+"any2";}
+
+
+
+
+
+
+
+// example
+let r, h;
+r = 5 + 6;
+h = r * 10;
+function easyWay4()
+{document.getElementById('easyCal4').innerHTML=h;}
+
+
+
+
+
+// show some
+function showtext()
+{document.getElementById('showig').innerHTML="Hello There";}
+
+
+
+
+
+
+
+// booleanTest
+function forbolf()
+{document.getElementById('foB').innerHTML=Boolean(1>2);}
+
+
+
+
+
+
+// booleanTest
+function forbolf2()
+{document.getElementById('foB2').innerHTML=Boolean(10<20);}
+
+
+
+
+
+// undercolor and text
+function clickColo()
+{document.getElementById('boxs').style.background=("red");}
+function mdo()
+{document.getElementById('fortex').innerHTML="Im New"}
+function mup()
+{document.getElementById('fortex').innerHTML="hello world"}
+function clickColo2()
+{document.getElementById('boxs').style.background=("black");}
+
+
+
+
+// width_borth
+function fowidth()
+{document.getElementById("forWidth").innerHTML = "Screen width is " + screen.width;}
+
+
+
+
+
+
+// height_borth
+function foHeight()
+{document.getElementById("forHeight").innerHTML = "Screen height is " + screen.height;}
+
+
+
+
+
+
+
+// forLoops
+let anyt="";
+
+for(i=0; i<5; i++)
+{
+    anyt+="hero" +i+"<hr>";
+    function foloops()
+    {document.getElementById('foloop').innerHTML=anyt;}
+}
+
+
+
+
+
+
+// forVar
+const shoping=100;
+const travel=200;
+let lost=shoping + travel;
+function foVar()
+{document.getElementById('forVar').innerHTML=+lost;}
+
+
+
+
+
+
+// forVar
+let calculetPluse = 10 + 80 + 10;
+function focalcu()
+{document.getElementById('forcal').innerHTML=calculetPluse;}
+
+
+
+
+
+
+
+// forVar
+let famnam="our"+ "" + "ssb571";
+function fonamcal()
+{document.getElementById('forNamcal').innerHTML=famnam;}
+
+
+
+
+
+
+
+// forVar
+let valuWname= 1 + 2 + "0";
+function fonumwc()
+{document.getElementById('forcwNamc').innerHTML=valuWname;}
+
+
+
+
+
+
+
+// forlet
+let viewv=5;
+{let viewv=2;}
+function forblok()
+{document.getElementById('foBlok').innerHTML=viewv;}
+
+
+
+
+
+
+
+// forVar
+var vnew=2;
+{var blockedv= 1;}
+function forblokv()
+{document.getElementById('foBlokV').innerHTML=blockedv;}
+
+
+
+
+
+
+// forVar
+const newCo = ["any1", "any2", "any3"]
+newCo [0]="any4";
+newCo.push("any5");
+function forcons()
+{document.getElementById('foCos').innerHTML=newCo;}
+
+
+
+
+
+
+
+
+// forconst
+const newCos=50;
+{const newCos=10;}
+function forConst()
+{document.getElementById('foConst').innerHTML="const is:"+newCos;}
+
+
+
+
+
+
+
+
+// forconst
+let carValu=5;
+let carcol=10;
+let totalValu = carValu * carcol;
+function forMalti()
+{document.getElementById('foMalti').innerHTML=totalValu;}
+
+
+
+
+
+
+
+// forArithmetic
+let oparwTorsf=5;
+let oparwTorss = (10 + 10) * oparwTorsf;
+function forArith()
+{document.getElementById('foArith').innerHTML=oparwTorss;}
+
+
+
+// =================
+// jqery_start
+// =================
+
+
+
+$(document).ready(function()
+{
+    $('#alte').on("click",function()
+    {
+        alert("Hello world");
+    });
+
+
+    $('#bot'+'#bot').on("click",function()
+    {
+        $(this).slideUp();
+    });
+
+    
+    $('.mbtn').on("click",function()
+    {
+        $("#imhide").hide();
+    });
+
+
+
+    $('.forBox').on("click",function()
+    {
+        $("#box12").hide();
+    });
+
+
+
+
+    $('.forBoxh').on("click",function()
+    {
+        $("#box13").height("300px");
+    });
+
+
+
+
+    $('.forBoxw').on("click",function()
+    {
+        $("#box14").width("300px");
+    });
+
+
+
+
+
+    $('#box15').on("mouseenter",function()
+    {
+        $("#box15").hide();
+    });
+
+
+
+
+
+
+    $('#bax16').on("mouseout",function()
+    {
+        $("#bax16").hide();
+    });
+
+
+
+
+
+    $('.winp').on("focus",function()
+    {
+        $(".winp").css("background-color","yellow");
+    });
+
+
+
+
+
+
+    $('.box17').on("mouseenter",function()
+    {
+        $(".box17").css("background-color", "black");
+    });
+    $('.box17').on("mouseleave",function()
+    {
+        $(".box17").css("background-color", "red");
+    });
+    $('.box17').on("click",function()
+    {
+        $(".box17").css("background-color", "green");
+    });
+
+
+
+
+
+
+
+    $('.box18').on("mouseenter",function()
+    {
+        $(".box18").css("background-color", "black");
+    });
+    $('.box18').on("mouseleave",function()
+    {
+        $(".box18").css("background-color", "red");
+    });
+    $('.box18').on("click",function()
+    {
+        $(".box18").hide();
+    });
+
+
+
+
+
+
+
+    $('.mmbtn').on("click",function()
+    {
+        $('#baxh').hide();
+    })
+
+
+
+
+
+
+
+
+
+    $('.mmbtnn').on("click",function()
+    {
+        $(".box19").hide();
+    });
+    $('.mmbtn').on("click",function()
+    {
+        $(".box19").show();
+    });
+
+
+
+
+
+
+
+
+    $('.mmbtns').on("click",function()
+    {
+        $(".box20").hide("1px");
+    });
+
+    $('.mmbts2').on("click",function()
+    {
+        $(".box20").show("1px");
+    });
+
+
+
+
+
+
+
+    $('.tog').on("click",function()
+    {
+        $(".box21").toggle("5000px");
+    });
+
+
+
+
+
+
+
+
+    $('.fad22').on("click",function()
+    {
+        $(".box22").fadeIn("");
+        $(".box23").fadeIn("slow");
+        $(".box24").fadeIn("3000");
+    });
+
+
+
+
+
+
+    $('.fadou').on("click",function()
+    {
+        $(".box25").fadeOut("");
+        $(".box26").fadeOut("slow");
+        $(".box27").fadeOut("3000");
+    });
+
+
+
+
+
+
+
+    $('.fadtog').on("click",function()
+    {
+        $(".box28").fadeToggle("");
+        $(".box29").fadeToggle("3000");
+        $(".box30").fadeToggle("slow");
+    });
+
+
+
+
+
+    $('.fadto').on("click",function()
+    {
+        $(".box31").fadeTo("slow", 0.20);
+        $(".box32").fadeTo("slow", 0.5);
+        $(".box33").fadeTo("slow", 0.9);
+    });
+
+
+
+
+
+
+    $('.box34').on("click",function()
+    {
+        $(".box35").slideToggle();
+    });
+
+
+
+
+
+    $('.bbt').on("click",function()
+    {
+        $(".box36").animate({right: '250px'});
+    });
+
+
+
+
+
+    $('.bts').on("click",function()
+    {
+        $(".box37").animate({right: '50px',width:'190px',height:'190px'});
+    });
+
+
+
+
+    $('.btshw').on("click",function()
+    {
+        $(".box39").animate({height: '300px', opacity: '0.4'}, "slow");
+        $(".box39").animate({height: '100px', opacity: '0.4'}, "slow");
+    });
+
+
+
+
+    $('.btsw').on("click",function()
+    {
+        $(".box40").animate({width: '300px', opacity: '0.8'}, "slow");
+        $(".box40").animate({width: '100px', opacity: '0.8'}, "slow");
+    });
+
+
+
+
+
+
+
+
+    $('.bthw').on("click",function()
+    {
+        $(".box41").animate({height: '300px', opacity: '0.4'}, "slow");
+        $(".box41").animate({width: '300px', opacity: '0.8'}, "slow");
+        $(".box41").animate({height: '100px', opacity: '0.4'}, "slow");
+        $(".box41").animate({width: '100px', opacity: '0.8'}, "slow");
+    });
+
+
+
+
+
+
+
+
+
+    $('.btf').on("click",function()
+    {
+        $(".box42").animate({left: '250px',width:'190px',height:'190px'});
+        $(".box42").animate({fontSize: '30px' ,left: '250px',width:'190px',height:'190px'});
+    });
+
+
+
+
+
+
+
+
+    $('.btt').on("click",function()
+    {
+        $(".box43").animate({height: 'toggle'});
+    });
+
+
+
+
+
+
+
+
+
+    $(".box44").click(function(){
+        $(".box45").slideDown(5000);
+    });
+    $(".btss").click(function(){
+        $(".box45").stop();
+    });
+
+
+
+
+
+
+
+    $(".start").click(function(){
+        $(".box46").animate({right: '100px'}, 5000);
+        $(".box46").animate({fontSize: '2em'}, 5000);
+    });
+    
+    $(".stop").click(function(){
+    $(".box46").stop();
+    });
+
+    $(".stop2").click(function(){
+    $(".box46").stop(true);
+    });
+
+    $(".stop3").click(function(){
+    $(".box46").stop(true, true);
+    }); 
+
+
+
+    $(".bbtt").on('click', function()
+    {
+        $('.box47').hide('slow',function()
+        {
+            alert("im off");
+        });
+    });
+
+
+
+
+
+    $(".bbth").on('click', function()
+    {
+        $('.box48').hide('slow')
+        alert("im off");
+    });
+
+
+
+
+
+
+    $(".bbssh").on('click', function()
+    {
+        $(".box49").css("color", "red").slideUp(2000).slideDown(2000);
+    });
+
+
+
+
+
+    $(".bfal").on('click', function()
+    {
+        alert("Text: " + $(".box50").text());
+    });
+    $(".bfal2").on('click', function()
+    {
+        alert("HTML: " + $(".box50").html());
+    });
+
+
+
+
+
+    $(".bfp").on('click', function()
+    {
+        alert("Value: " + $(".box51").val());
+    });
+
+
+
+
+
+    $(".yout").on('click', function()
+    {
+        alert("Value: " + $(".ytb").val());
+    });
+
+
+
+
+
+
+    $(".fform").on('click', function()
+    {
+        $(".fort1").text("Hello world!");
+    });
+    $(".fform2").on('click', function()
+    {
+        $(".fort2").html("<b>Hello world!</b>");
+    });
+    $(".fform3").on('click', function()
+    {
+        $(".fort").val("I'm on");
+    });
+
+
+
+
+
+    $(".ffodg").on('click', function()
+    {
+        $(".forgh1").text(function(i, origText){
+            return "Old text: " + origText + " New text: Hello world! (index: " + i + ")";
+    });
+    });
+
+    $(".ffog").on('click', function()
+    {
+        $(".forgh2").text(function(i, origText){
+            return "Old html: " + origText + " New html: Hello <b>world!</b> (index: " + i + ")"; 
+    });
+    });
+
+
+
+
+    $(".mmb1").on('click', function()
+    {
+        $(".heed3").append(" <b>Appended text</b>.");
+    });
+    $(".mmb2").on('click', function()
+    {
+        $("ol").append("<li>Appended item</li>");
+    });
+
+
+
+
+    $(".mmb3").on('click', function()
+    {
+        $(".heed4").prepend(" <b>Appended text</b>.");
+    });
+    $(".mmb4").on('click', function()
+    {
+        $(".olga").append("<li>Appended item</li>");
+    });
+
+
+
+
+
+    $(".mmb4").on('click', function()
+    {
+        $(".imimg").before("<b>Before</b>");
+    });
+    $(".mmb5").on('click', function()
+    {
+        $(".imimg").after("<i>After</i>");
+    });
+
+
+
+
+    $(".mmb7").on('click', function()
+    {
+        $(".mmb6").remove();
+    });
+
+
+
+
+    $(".mmb9").on('click', function()
+    {
+        $(".mmb8").empty();
+    });
+
+
+
+
+    $(".mmb11").on('click', function()
+    {
+        $(".con").addClass("blue");
+        $(".mmb10").addClass("important");
+    });
+
+
+
+
+
+
+
+    $(".mmb13").on('click', function()
+    {
+        $(".mmb12").addClass("importants blues");
+    });
+
+
+
+
+
+
+
+
+    $(".mmb14").on('click', function()
+    {
+        $(".pain").removeClass("colert");
+    });
+
+
+
+
+
+    $(".mmb15").on('click', function()
+    {
+        $(".pains").toggleClass("colertt");
+    });
+
+
+
+
+    $(".mmb16").on('click', function()
+    {
+        alert("Background color = " + $(".newcon").css("background-color"));
+    });
+
+
+
+    $(".mmb17").on('click', function()
+    {
+        $(".newcon1").css("background-color", "yellow");
+        
+    });
+
+
+
+
+    $(".mmb18").on('click', function()
+    {
+        $(".newcon2").css({"background-color": "silver", "font-size": "200%"});
+        
+    });
+
+
+
+
+    $(".mmb19").on('click', function()
+    {
+        $(".fnd").width(500).height(500);
+        
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
